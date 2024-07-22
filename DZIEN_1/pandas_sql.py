73a89336f65037bbb86fc9af32280dafdb9f8b9e
albim72 <marcin.albiniak72@gmail.com>
2024-07-22 15:46:45 +0200

    Create pandas_sql.py

---
+++ DZIEN_1/pandas_sql.py
@@ -0,0 +1,7 @@
+import mysql.connector
+import pandas as pd
+
+db = mysql.connector.connect(user='root',password='abc123',host='127.0.0.1',port=3306,database='bookstore')
+ramka = pd.read_sql('SELECT * FROM books',con=db)
+
+print(ramka)
